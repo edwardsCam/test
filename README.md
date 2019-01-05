26742499f0dbfc593a937b77d184eaf074ca181b
Cameron Edwards <edwards.cameron.pc@gmail.com>
2019-01-05 16:17:47 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Matador
+
+todo
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './App.scss'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        {'hello'}
+      </div>
+    )
+  }
+}
